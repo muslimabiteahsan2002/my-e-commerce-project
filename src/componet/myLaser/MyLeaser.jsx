082891a6e39b65a09643d6e30d4c272695d2e3ee
MUSLIMA <muslimabintea@gmail.com>
2026-01-13 23:49:33 +0600

    Laser Jet Printer Model 31

--- src/componet/myLaser/MyLeaser.jsx
+++ src/componet/myLaser/MyLeaser.jsx
@@ -5,9 +5,334 @@ class MyLeaser extends Component {
     render() {
         return (
             <Fragment>
-                <Container>
+                <Container className='allInfor'>
                     <div className='My_table'>
-                        <table></table>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>SL</th>
+                                    <th>Laser Jet Printer Model</th>
+                                    <th>Toner Model</th>
+                                    <th>Economy Price China</th>
+                                    <th>Medium Price China</th>
+                                    <th>Best Price China</th>
+                                    <th>Original Toner Price</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>HP P2030, 2035, 2035n, 2050, 2055d, P2055n, 2055x, 400, M401, M401dn, M425</td>
+                                    <td>05A/80A/319/719</td>
+                                    <td>1,000</td>
+                                    <td>1,200</td>
+                                    <td>1,500</td>
+                                    <td>14,000</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>HP LaserJet P2035, P2035n, P2055dn, P2055x</td>
+                                    <td>05X/80X</td>
+                                    <td>1,700</td>
+                                    <td>1,900</td>
+                                    <td>2,200</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>HP P2030, 2035, 2035n, 2050, 2055d, P2055n, 2055x, 400, M401, M401dn, M425 (Double version of 05A/80A)</td>
+                                    <td>05X/80X/319X/719X</td>
+                                    <td>1,700</td>
+                                    <td>1,900</td>
+                                    <td>2,200</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td>Canon LBP 3100, 3150, 3050 printers</td>
+                                    <td>326</td>
+                                    <td>800</td>
+                                    <td>900</td>
+                                    <td>1,000</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>5</td>
+                                    <td>HP 2300, 2300N, 2300DN, 2300DTN</td>
+                                    <td>10A</td>
+                                    <td>1,800</td>
+                                    <td>2,000</td>
+                                    <td>2,200</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>6</td>
+                                    <td>HP 2400, 2410, 2420, 243, LBP 3410, 3460</td>
+                                    <td>11A/310</td>
+                                    <td>1,400</td>
+                                    <td>1,600</td>
+                                    <td>1,800</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>7</td>
+                                    <td>HP 1010, 1015, 1018, 1020, 1022, 3015, 3020, 3030, 3050, 3055, LBP 2900, 3000</td>
+                                    <td>12A/303/FX-9</td>
+                                    <td>750</td>
+                                    <td>950</td>
+                                    <td>1,000</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>8</td>
+                                    <td>HP 1300</td>
+                                    <td>13A</td>
+                                    <td>800</td>
+                                    <td>1,000</td>
+                                    <td>1,200</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>9</td>
+                                    <td>HP 1200, 1220, 3380, LBP-1210</td>
+                                    <td>15A/EP25</td>
+                                    <td>900</td>
+                                    <td>1,200</td>
+                                    <td>1,400</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>10</td>
+                                    <td>HP 5200, 5200n, 5200tn, 5200dtn, 5200le, LBP-3500, 3900, 3950</td>
+                                    <td>16A/309</td>
+                                    <td>2,000</td>
+                                    <td>2,300</td>
+                                    <td>2,500</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>11</td>
+                                    <td>HP M102a, M102w, M130a, M130fn, MFP M130fw, MFP M130nw</td>
+                                    <td>17A</td>
+                                    <td>1,300</td>
+                                    <td>1,500</td>
+                                    <td>1,800</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>12</td>
+                                    <td>HP M104, M104a, M104w, M132 MFP,M132a MFP,M132fn MFP,M132fw</td>
+                                    <td>18A</td>
+                                    <td>1,800</td>
+                                    <td>2,200</td>
+                                    <td>2,500</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>13</td>
+                                    <td>HP LaserJet Pro M102w, MFP M130fw</td>
+                                    <td>19A</td>
+                                    <td>1,500</td>
+                                    <td>1,800</td>
+                                    <td>2,200</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>14</td>
+                                    <td>HP 1150</td>
+                                    <td>24A</td>
+                                    <td>1,400</td>
+                                    <td>1,800</td>
+                                    <td>2,200</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>15</td>
+                                    <td>HP M402D, M402d, M402n, M402dn, M402dw, M402m, M402dne, M426fdw, M426dw (Price 26X 1,600/-)</td>
+                                    <td>26A/CRG-052</td>
+                                    <td>1,200</td>
+                                    <td>1,500</td>
+                                    <td>1,800</td>
+                                    <td>15,400</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>16</td>
+                                    <td>HP M402D, M402d, M402n, M402dn, M402dw, M402m, M402dne, M426fdw, M426dw</td>
+                                    <td>26X</td>
+                                    <td>1,800</td>
+                                    <td>2,000</td>
+                                    <td>2,200</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>17</td>
+                                    <td>HP M402D, M402d, M402n, M402dn, M402dw, M402m, M402dne, M426fdw, M426dw (Extended version of 26A)</td>
+                                    <td>26X</td>
+                                    <td>1,800</td>
+                                    <td>2,000</td>
+                                    <td>2,200</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>18</td>
+                                    <td>HP 4050, 4050n, 4050TN</td>
+                                    <td>27X</td>
+                                    <td>2,200</td>
+                                    <td>2,400</td>
+                                    <td>2,600</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>19</td>
+                                    <td>HP 5000, 5100N</td>
+                                    <td>29X</td>
+                                    <td>2,100</td>
+                                    <td>2,500</td>
+                                    <td>2,900</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>20</td>
+                                    <td>HP M203d, M203dn, M203dw, M227fdn, M227sdn, M227fdw, LBP 162dw</td>
+                                    <td>30A/051</td>
+                                    <td>1,200</td>
+                                    <td>1,500</td>
+                                    <td>1,800</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>21</td>
+                                    <td>HP M203d, M203dn, M203dw, M227fdn, M227sdn, M227fdw, LBP 162dw (Drum Unit of 30A)</td>
+                                    <td>32A</td>
+                                    <td>1,600</td>
+                                    <td>1,800</td>
+                                    <td>2,200</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>22</td>
+                                    <td>HP P1005, P11006, LBP-3010, 3018, 3050, 3100, 3108, 3150, P1505, M1120, M522, P1566, 1567, 1568, 1569, 1606, 1606dn, HP P1005, P1006, P1100, P1102, P1102W, P1503, P1506, M1132, LBP 6000, 6030, 6200, 3018, 3010, M1214</td>
+                                    <td>35A/36A/78A/85A/312/313/<br/>325/326/512/513/712/713/912/913</td>
+                                    <td>700</td>
+                                    <td>900</td>
+                                    <td>1,000</td>
+                                    <td>4,400</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>23</td>
+                                    <td>HP P1505, M1120, M522</td>
+                                    <td>36A</td>
+                                    <td>800</td>
+                                    <td>1,000</td>
+                                    <td>1,100</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>24</td>
+                                    <td>HP P1505, M1120, M522 (Extended version of 36A)</td>
+                                    <td>36X</td>
+                                    <td>1,200</td>
+                                    <td>1,400</td>
+                                    <td>1,600</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>25</td>
+                                    <td>HP M631dn M632fth, M633z, M633fh, M607dn, M607n, M608n,M608dn, M608x, M609</td>
+                                    <td>37A</td>
+                                    <td>3,800</td>
+                                    <td>4,200</td>
+                                    <td>4,500</td>
+                                    <td>23,600</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>26</td>
+                                    <td>HP 4200, 4200L, 4240, 4250, 4250n, 4350, 4350dtn</td>
+                                    <td>38A/42A</td>
+                                    <td>2,900</td>
+                                    <td>3,200</td>
+                                    <td>3,500</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>27</td>
+                                    <td>HP 4300</td>
+                                    <td>39A</td>
+                                    <td>4,300</td>
+                                    <td>4,500</td>
+                                    <td>4,900</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>28</td>
+                                    <td>HP 9000, 9040, 9050</td>
+                                    <td>43X</td>
+                                    <td>Call</td>
+                                    <td>Call</td>
+                                    <td>Call</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>29</td>
+                                    <td>HP M28W, M16, M15a, M15w, MFP28a, MFP28w, MFP29w [44X/48X Price 1800/-]</td>
+                                    <td>44A/48A</td>
+                                    <td>1,200</td>
+                                    <td>1,500</td>
+                                    <td>1,800</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>30</td>
+                                    <td>HP M28W, M16, M15a, M15w, MFP28a, MFP28w, MFP29w [Extended version of 44A/48A]</td>
+                                    <td>44X/48X</td>
+                                    <td>1,400</td>
+                                    <td>1,700</td>
+                                    <td>2,000</td>
+                                    <td>Call</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>31</td>
+                                    <td>HP P2011, P2011n, P2012, P2012n, P2013, P2013n, P2014, P2014n, P2014n, P2015d, P2015n, P2015dn, P2015x, M2727nf, M2727nfs, HP 1160, 1320N, 1320TN, 3390MFP, 3392MFP, LBP-3300, 3360</td>
+                                    <td>49A/53A/308/315/508/515/708</td>
+                                    <td>1,000</td>
+                                    <td>1,300</td>
+                                    <td>1,800</td>
+                                    <td>7,700</td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </Container>
             </Fragment>
